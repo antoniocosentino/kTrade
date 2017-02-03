4e6a1052bff1757fede7d513186eba98a445fc0b
Antonio Cosentino <hello@antoniocosentino.it>
2017-02-03 16:32:30 +0100

    new version 1.1

--- cbot.cs
+++ cbot.cs
@@ -16,6 +16,11 @@ namespace cAlgo
         private int pointscounter;
         private bool is_position_open;
         private string position_type;
+        private string stringdate;
+        private int currenthour;
+        private int currentminute;
+        private int currentslotnumber;
+        private bool botactive;
 
         [Parameter("Contracts (EURO)", DefaultValue = 100000, MinValue = 1000, MaxValue = 100000000)]
         public int ncontracts { get; set; }
@@ -23,24 +28,466 @@ namespace cAlgo
         [Parameter("Candles Sequence", DefaultValue = 2, MinValue = 1, MaxValue = 20)]
         public int csequence { get; set; }
 
-        public static List<string> candleslist = new List<string>();
+        // Time Parameters
+        [Parameter("0:00 - 0:29", DefaultValue = true)]
+        public bool timeslot_00 { get; set; }
 
+        [Parameter("0:30 - 0:59", DefaultValue = true)]
+        public bool timeslot_01 { get; set; }
+
+        [Parameter("1:00 - 1:29", DefaultValue = true)]
+        public bool timeslot_02 { get; set; }
+
+        [Parameter("1:30 - 1:59", DefaultValue = true)]
+        public bool timeslot_03 { get; set; }
+
+        [Parameter("2:00 - 2:29", DefaultValue = true)]
+        public bool timeslot_04 { get; set; }
+
+        [Parameter("2:30 - 2:59", DefaultValue = true)]
+        public bool timeslot_05 { get; set; }
+
+        [Parameter("3:00 - 3:29", DefaultValue = true)]
+        public bool timeslot_06 { get; set; }
+
+        [Parameter("3:30 - 3:59", DefaultValue = true)]
+        public bool timeslot_07 { get; set; }
+
+        [Parameter("4:00 - 4:29", DefaultValue = true)]
+        public bool timeslot_08 { get; set; }
+
+        [Parameter("4:30 - 4:59", DefaultValue = true)]
+        public bool timeslot_09 { get; set; }
+
+        [Parameter("5:00 - 5:29", DefaultValue = true)]
+        public bool timeslot_10 { get; set; }
+
+        [Parameter("5:30 - 5:59", DefaultValue = true)]
+        public bool timeslot_11 { get; set; }
+
+        [Parameter("6:00 - 6:29", DefaultValue = true)]
+        public bool timeslot_12 { get; set; }
+
+        [Parameter("6:30 - 6:59", DefaultValue = true)]
+        public bool timeslot_13 { get; set; }
+
+        [Parameter("7:00 - 7:29", DefaultValue = true)]
+        public bool timeslot_14 { get; set; }
+
+        [Parameter("7:30 - 7:59", DefaultValue = true)]
+        public bool timeslot_15 { get; set; }
+
+        [Parameter("8:00 - 8:29", DefaultValue = true)]
+        public bool timeslot_16 { get; set; }
+
+        [Parameter("8:30 - 8:59", DefaultValue = true)]
+        public bool timeslot_17 { get; set; }
+
+        [Parameter("9:00 - 9:29", DefaultValue = true)]
+        public bool timeslot_18 { get; set; }
+
+        [Parameter("9:30 - 9:59", DefaultValue = true)]
+        public bool timeslot_19 { get; set; }
+
+        [Parameter("10:00 - 10:29", DefaultValue = true)]
+        public bool timeslot_20 { get; set; }
+
+        [Parameter("10:30 - 10:59", DefaultValue = true)]
+        public bool timeslot_21 { get; set; }
+
+        [Parameter("11:00 - 11:29", DefaultValue = true)]
+        public bool timeslot_22 { get; set; }
+
+        [Parameter("11:30 - 11:59", DefaultValue = true)]
+        public bool timeslot_23 { get; set; }
+
+        [Parameter("12:00 - 12:29", DefaultValue = true)]
+        public bool timeslot_24 { get; set; }
+
+        [Parameter("12:30 - 12:59", DefaultValue = true)]
+        public bool timeslot_25 { get; set; }
+
+        [Parameter("13:00 - 13:29", DefaultValue = true)]
+        public bool timeslot_26 { get; set; }
+
+        [Parameter("13:30 - 13:59", DefaultValue = true)]
+        public bool timeslot_27 { get; set; }
+
+        [Parameter("14:00 - 14:29", DefaultValue = true)]
+        public bool timeslot_28 { get; set; }
+
+        [Parameter("14:30 - 14:59", DefaultValue = true)]
+        public bool timeslot_29 { get; set; }
+
+        [Parameter("15:00 - 15:29", DefaultValue = true)]
+        public bool timeslot_30 { get; set; }
+
+        [Parameter("15:30 - 15:59", DefaultValue = true)]
+        public bool timeslot_31 { get; set; }
+
+        [Parameter("16:00 - 16:29", DefaultValue = true)]
+        public bool timeslot_32 { get; set; }
+
+        [Parameter("16:30 - 16:59", DefaultValue = true)]
+        public bool timeslot_33 { get; set; }
+
+        [Parameter("17:00 - 17:29", DefaultValue = true)]
+        public bool timeslot_34 { get; set; }
+
+        [Parameter("17:30 - 17:59", DefaultValue = true)]
+        public bool timeslot_35 { get; set; }
+
+        [Parameter("18:00 - 18:29", DefaultValue = true)]
+        public bool timeslot_36 { get; set; }
+
+        [Parameter("18:30 - 18:59", DefaultValue = true)]
+        public bool timeslot_37 { get; set; }
+
+        [Parameter("19:00 - 19:29", DefaultValue = true)]
+        public bool timeslot_38 { get; set; }
+
+        [Parameter("19:30 - 19:59", DefaultValue = true)]
+        public bool timeslot_39 { get; set; }
+
+        [Parameter("20:00 - 20:29", DefaultValue = true)]
+        public bool timeslot_40 { get; set; }
+
+        [Parameter("20:30 - 20:59", DefaultValue = true)]
+        public bool timeslot_41 { get; set; }
+
+        [Parameter("21:00 - 21:29", DefaultValue = true)]
+        public bool timeslot_42 { get; set; }
+
+        [Parameter("21:30 - 21:59", DefaultValue = true)]
+        public bool timeslot_43 { get; set; }
+
+        [Parameter("22:00 - 22:29", DefaultValue = true)]
+        public bool timeslot_44 { get; set; }
+
+        [Parameter("22:30 - 22:59", DefaultValue = true)]
+        public bool timeslot_45 { get; set; }
+
+        [Parameter("23:00 - 23:29", DefaultValue = true)]
+        public bool timeslot_46 { get; set; }
+
+        [Parameter("23:30 - 23:59", DefaultValue = true)]
+        public bool timeslot_47 { get; set; }
+
+        //
+
+        public static List<string> candleslist = new List<string>();
+        public static List<int> blackslots = new List<int>();
 
         protected override void OnStart()
         {
             // Put your initialization logic here
-            Print("kTrade Started");
+            Print("kTrade 1.1 started");
+            Print("Server time is {0}", Server.Time.AddHours(1));
             _heikenAshi = Indicators.GetIndicator<HeikenAshi>(1);
             kcounter = 0;
             is_position_open = false;
             position_type = null;
+            stringdate = "";
+            currenthour = 0;
+            currentminute = 0;
+            currentslotnumber = 0;
+            botactive = false;
+
+            // Adding "black" hours
+
+            if (!timeslot_00)
+            {
+                blackslots.Add(0);
+            }
+
+            if (!timeslot_01)
+            {
+                blackslots.Add(1);
+            }
+
+            if (!timeslot_02)
+            {
+                blackslots.Add(2);
+            }
+
+            if (!timeslot_03)
+            {
+                blackslots.Add(3);
+            }
+
+            if (!timeslot_04)
+            {
+                blackslots.Add(4);
+            }
+
+            if (!timeslot_05)
+            {
+                blackslots.Add(5);
+            }
+
+            if (!timeslot_06)
+            {
+                blackslots.Add(6);
+            }
+
+            if (!timeslot_07)
+            {
+                blackslots.Add(7);
+            }
+
+            if (!timeslot_08)
+            {
+                blackslots.Add(8);
+            }
+
+            if (!timeslot_09)
+            {
+                blackslots.Add(9);
+            }
+
+            if (!timeslot_10)
+            {
+                blackslots.Add(10);
+            }
+
+            if (!timeslot_11)
+            {
+                blackslots.Add(11);
+            }
+
+            if (!timeslot_12)
+            {
+                blackslots.Add(12);
+            }
+
+            if (!timeslot_13)
+            {
+                blackslots.Add(13);
+            }
+
+            if (!timeslot_14)
+            {
+                blackslots.Add(14);
+            }
+
+            if (!timeslot_15)
+            {
+                blackslots.Add(15);
+            }
+
+            if (!timeslot_16)
+            {
+                blackslots.Add(16);
+            }
+
+            if (!timeslot_17)
+            {
+                blackslots.Add(17);
+            }
+
+            if (!timeslot_18)
+            {
+                blackslots.Add(18);
+            }
+
+            if (!timeslot_19)
+            {
+                blackslots.Add(19);
+            }
+
+            if (!timeslot_20)
+            {
+                blackslots.Add(20);
+            }
+
+            if (!timeslot_21)
+            {
+                blackslots.Add(21);
+            }
+
+            if (!timeslot_22)
+            {
+                blackslots.Add(22);
+            }
+
+            if (!timeslot_23)
+            {
+                blackslots.Add(23);
+            }
+
+            if (!timeslot_24)
+            {
+                blackslots.Add(24);
+            }
+
+            if (!timeslot_25)
+            {
+                blackslots.Add(25);
+            }
+
+            if (!timeslot_26)
+            {
+                blackslots.Add(26);
+            }
+
+            if (!timeslot_27)
+            {
+                blackslots.Add(27);
+            }
+
+            if (!timeslot_28)
+            {
+                blackslots.Add(28);
+            }
+
+            if (!timeslot_29)
+            {
+                blackslots.Add(29);
+            }
+
+            if (!timeslot_30)
+            {
+                blackslots.Add(30);
+            }
+
+            if (!timeslot_31)
+            {
+                blackslots.Add(31);
+            }
+
+            if (!timeslot_32)
+            {
+                blackslots.Add(32);
+            }
+
+            if (!timeslot_33)
+            {
+                blackslots.Add(33);
+            }
+
+            if (!timeslot_34)
+            {
+                blackslots.Add(34);
+            }
+
+            if (!timeslot_35)
+            {
+                blackslots.Add(35);
+            }
+
+            if (!timeslot_36)
+            {
+                blackslots.Add(36);
+            }
+
+            if (!timeslot_37)
+            {
+                blackslots.Add(37);
+            }
+
+            if (!timeslot_38)
+            {
+                blackslots.Add(38);
+            }
+
+            if (!timeslot_39)
+            {
+                blackslots.Add(39);
+            }
+
+            if (!timeslot_40)
+            {
+                blackslots.Add(40);
+            }
+
+            if (!timeslot_41)
+            {
+                blackslots.Add(41);
+            }
+
+            if (!timeslot_42)
+            {
+                blackslots.Add(42);
+            }
+
+            if (!timeslot_43)
+            {
+                blackslots.Add(43);
+            }
+
+            if (!timeslot_44)
+            {
+                blackslots.Add(44);
+            }
+
+            if (!timeslot_45)
+            {
+                blackslots.Add(45);
+            }
+
+            if (!timeslot_46)
+            {
+                blackslots.Add(46);
+            }
+
+            if (!timeslot_47)
+            {
+                blackslots.Add(47);
+            }
+
+            // "black" hours setup end
+
         }
 
         protected override void OnBar()
         {
             kcounter++;
             pointscounter = 0;
-            Print("Counter values is {0}", kcounter);
+
+            stringdate = Server.Time.AddHours(1).ToString("HH:mm");
+
+            currenthour = int.Parse(stringdate.Substring(0, 2));
+            currenthour = Convert.ToInt32(stringdate.Substring(0, 2));
+
+            currentminute = int.Parse(stringdate.Substring(3, 2));
+            currentminute = Convert.ToInt32(stringdate.Substring(3, 2));
+
+            if (currentminute >= 30)
+            {
+                currentslotnumber = (currenthour * 2) + 1;
+            }
+            else
+            {
+                currentslotnumber = currenthour * 2;
+            }
+
+
+            if (blackslots.Any(item => item == currentslotnumber))
+            {
+                botactive = false;
+
+                if (is_position_open)
+                {
+                    Print("Closing active position");
+                    var position = Positions.Find("kTrade");
+                    ClosePosition(position);
+                    is_position_open = false;
+                    position_type = null;
+                }
+
+
+            }
+            else
+            {
+                botactive = true;
+            }
+
+
+
+
+
+
+
 
             if (_heikenAshi.xOpen.Last(1) < _heikenAshi.xClose.Last(1))
             {
@@ -78,20 +525,22 @@ namespace cAlgo
             {
                 if (pointscounter == csequence || pointscounter == (csequence * -1))
                 {
-                    Print("opening position");
-                    is_position_open = true;
-
-                    if (pointscounter == csequence)
+                    if (botactive)
                     {
-                        var result = ExecuteMarketOrder(TradeType.Buy, Symbol, ncontracts, "kTrade");
-                        position_type = "buy";
-                    }
-                    else
-                    {
-                        var result = ExecuteMarketOrder(TradeType.Sell, Symbol, ncontracts, "kTrade");
-                        position_type = "sell";
-                    }
+                        Print("Opening position");
+                        is_position_open = true;
 
+                        if (pointscounter == csequence)
+                        {
+                            var result = ExecuteMarketOrder(TradeType.Buy, Symbol, ncontracts, "kTrade");
+                            position_type = "buy";
+                        }
+                        else
+                        {
+                            var result = ExecuteMarketOrder(TradeType.Sell, Symbol, ncontracts, "kTrade");
+                            position_type = "sell";
+                        }
+                    }
                 }
             }
 
@@ -102,30 +551,30 @@ namespace cAlgo
                 {
                     if (pointscounter == csequence && position_type == "sell")
                     {
-                        Print("closing position");
+                        Print("Closing position");
                         if (position != null)
                         {
                             ClosePosition(position);
                         }
-                        Print("reversing position");
+                        Print("Reversing position");
                         var result = ExecuteMarketOrder(TradeType.Buy, Symbol, ncontracts, "kTrade");
                         position_type = "buy";
 
                     }
                     else if (pointscounter == (csequence * -1) && position_type == "buy")
                     {
-                        Print("closing position");
+                        Print("Closing position");
                         if (position != null)
                         {
                             ClosePosition(position);
                         }
-                        Print("reversing position");
+                        Print("Reversing position");
                         var result = ExecuteMarketOrder(TradeType.Sell, Symbol, ncontracts, "kTrade");
                         position_type = "sell";
                     }
                     else
                     {
-                        Print("keep position open");
+                        // Keep position open
                     }
                 }
             }
